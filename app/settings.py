8343eea83da2784b85991182d6506db6365b9629
gmdiniz <gabrieldinizmota@hotmail.com>
2022-04-05 22:33:28 -0300

    deploy v2

--- app/settings.py
+++ app/settings.py
@@ -135,8 +135,10 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
